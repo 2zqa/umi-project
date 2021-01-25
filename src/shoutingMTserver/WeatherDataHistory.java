f5a40b304fca0fae322a4ac113c26cdb2aba37a8
2zqa <25235249+2zqa@users.noreply.github.com>
2021-01-25 12:28:15 +0100

    Update WeatherDataHistory.java

--- src/shoutingMTserver/WeatherDataHistory.java
+++ src/shoutingMTserver/WeatherDataHistory.java
@@ -1,8 +1,11 @@
 package shoutingMTserver;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 
-public class WeatherDataHistory {
+public class WeatherDataHistory implements Iterable<WeatherData> {
     private ArrayList<WeatherData> history;
     private int capacity;
 
@@ -11,11 +14,25 @@ public class WeatherDataHistory {
         this.capacity = capacity;
     }
 
-    public void add(WeatherData weatherData) {
+    /**
+     * Adds data to history. Cannot be run at the same time or the capacity could overload
+     * @param weatherData weatherdata object to add
+     */
+    public synchronized void add(WeatherData weatherData) {
         // If history is full, remove last item before adding new one
         if(history.size() >= capacity) {
             history.remove(0);
         }
         history.add(weatherData);
+    }
+
+    public int size() {
+        return history.size();
+    }
+
+
+    @Override
+    public Iterator<WeatherData> iterator() {
+        return history.iterator();
     }
 }
